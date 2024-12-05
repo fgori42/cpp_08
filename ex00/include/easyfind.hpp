788515a7352a14802a757e96d13dd6152cb94001
fgori42 <98gori@gmail.com>
2024-12-05 15:45:59 +0100

    ....

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -1,6 +1,20 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <iostream>
+# include <algorithm>
+# include <exception>
+# include <stdexcept>
+
+class ImpossibleToFind : public exception
+{
+	public:
+		const char *what() const throw()
+		{
+			return "not in the vector";
+		}
+};
+
 template<typename T>
 void	easyfind(std::vector<T> &cont, int n);
 
--- ex00/include/easyfind.tpp
+++ ex00/include/easyfind.tpp
@@ -3,5 +3,9 @@
 template<typename T>
 void	easyfind(std::vector<T> &cont, int n)
 {
-	
+	typename std::vector<T>::iterator iter = std::find(cont.begin(), cont.end(), n);
+	if (iter == cont.end())
+		throw ImpossibleToFind();
+	else
+		return(iter);
 }
